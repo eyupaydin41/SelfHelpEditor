c08bdf1957e4ffeef51d28a442b57f21dd02f7cc
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-08-30 01:45:32 +0300

    Implement singleton pattern for MongoDB connection class

---
+++ config/db.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+let instance = null;
+
+class Database { 
+
+    constructor() {
+        if (instance === null) {   
+            this.connectDB(); 
+            instance = this;
+        }
+        return instance;
+    }
+    
+    connectDB = async () => {
+        try {
+            await mongoose.connect(process.env.CONNECTION_STRING);
+            console.log('MongoDB connected');
+        } catch (err) {
+            console.error(err.message);
+            process.exit(1);        
+        }
+    };
+}
+
+const dbInstance = new Database();
+Object.freeze(dbInstance);
+
+module.exports = dbInstance; 

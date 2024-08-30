95b04c8d12c4f7f7c4372a766050b9149a1a15cc
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-08-31 00:46:23 +0300

    Add "auth" router

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var contentRouter = require('./routes/content');
+var authRouter = require(`./routes/auth`);
 
 var app = express();
 
@@ -20,6 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/auth', authRouter);
 app.use('/', indexRouter);
 app.use('/content', contentRouter);
 
---
+++ routes/auth.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+router.get('/', (req, res) => {
+    res.render('auth');
+});
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.findOne({ username });
+
+        if (!user) {
+            return res.status(401).json({ message: 'Kullanıcı bulunamadı' });
+        }
+
+        const isMatch = await user.comparePassword(password);
+
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Şifre yanlış' });
+        }
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_TOKEN, { expiresIn: '1h' });
+
+        res.cookie('token', token, { httpOnly: true, secure: true, maxAge: 3600000 });
+
+        res.status(200).json({ message: 'Giriş başarılı' });
+    } catch (err) {
+        console.error('Hata:', err);
+        res.status(500).json({ message: 'Bir hata oluştu' });
+    }
+});
+
+router.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({ message: 'Bu kullanıcı adı zaten alınmış' });
+        }
+
+        const newUser = new User({ username, password });
+        await newUser.save();
+
+        const token = jwt.sign({ userId: newUser._id }, process.env.JWT_TOKEN, { expiresIn: '1h' });
+
+        res.status(201).json({ token, message: 'Kayıt başarılı' });
+    } catch (err) {
+        console.error('Register Hatası:', err);
+        res.status(500).json({ message: 'Bir hata oluştu. Kayıt yapılamadı.' });
+    }
+});
+
+module.exports = router;
